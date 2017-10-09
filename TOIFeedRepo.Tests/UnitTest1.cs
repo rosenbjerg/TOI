62acc0e48997d7ad3192a86bbf65d370a0be2ca3
Jonas Kloster Jacobsen <joklost@gmail.com>
2017-10-09 09:26:03 +0200

    Adds initial project structures.

---
+++ TOIFeedRepo.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TOIFeedRepo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TOIWebClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TOIWebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
