53af963404dc09c5ce9f450d803f113c1814eaec
Malte Rosenbjerg <malterandersen@live.dk>
2017-10-09 09:10:04 +0200

    solution init

---
+++ TOIserver/TOIserver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TOIserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
