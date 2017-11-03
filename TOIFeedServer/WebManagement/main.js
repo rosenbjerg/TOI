2f185d5b7e6727b701837fa9ff7c7275d4f3e52c
Jesper Nielsen <jesper@windelborg.dk>
2017-11-03 12:39:23 +0100

    Added template engine

--- TOIFeedServer/WebManagement/main.js
+++ TOIFeedServer/WebManagement/main.js
@@ -1,15 +1,40 @@
-$("#mapPicker").locationpicker({
-    location: {
-        latitude: 57.012392,
-        longitude: 9.991556
-    },
-    radius: 200,
-    enableAutocomplete: true,
-    inputBinding: {
-        latitudeInput: $("#latitudeInput"),
-        longitudeInput: $("#longitudeInput"),
-        radiusInput: $("#radiusInput"),
-        locationNameInput: $("#locationNameInput")
+
+let $container = $(".container");
+let templates = {
+    saveEditTag : JsT.loadById("saveEditTag")
+}
+
+function showSaveEditTag(tag) {
+    if (arguments.length === 0){
+        $container.empty().append(templates.saveEditTag.render({
+            action: "Create"
+        }));
     }
-});
-$("#mapPicker").locationpicker("autosize");
+    else {
+
+        $container.empty().append(templates.saveEditTag.render({
+            action: "Edit"
+        }));
+    }
+    initMapPicker();
+}
+
+function initMapPicker() {
+    $("#mapPicker").locationpicker({
+        location: {
+            latitude: 57.012392,
+            longitude: 9.991556
+        },
+        radius: 200,
+        enableAutocomplete: true,
+        inputBinding: {
+            latitudeInput: $("#latitudeInput"),
+            longitudeInput: $("#longitudeInput"),
+            radiusInput: $("#radiusInput"),
+            locationNameInput: $("#locationNameInput")
+        }
+    });
+    $("#mapPicker").locationpicker("autosize");
+}
+
+showSaveEditTag();
