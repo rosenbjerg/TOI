72abc6d0a63d526c47b6374893a07472d3bc1b38
Tobias Morell <tmorel14@student.aau.dk>
2017-11-15 11:30:26 +0100

    Added interface for mongodb abstraction

---
+++ TOIFeedServer/Database/IDbCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOIFeedServer.Database
+{
+    public interface IDbCollection<T>
+    {
+        Task<DatabaseStatusCode> Insert(params T[] items);
+        Task<DatabaseStatusCode> Update(string id, T item);
+        Task<DatabaseStatusCode> Delete(string id);
+        Task<DbResult<IEnumerable<T>>> Find(Expression<Func<T, bool>> predicate);
+        Task<DbResult<T>> FindOne(Expression<Func<T, bool>> predicate);
+        Task<DbResult<T>> FindOne(string id);
+        Task<DbResult<IEnumerable<T>>> GetAll();
+    }
+}
