e6d676f11ff34c6b3edbb8ab25ff47d04f1933dc
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2017-12-07 15:22:43 +0100

    get status of feed (feedrepo)

---
+++ TOIFeedRepo/ApiKeyGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using TOIFeedRepo.Database;
+
+namespace TOIFeedRepo
+{
+    class ApiKeyGenerator
+    {
+        private FeedRepoDatabase _db;
+        private readonly RandomNumberGenerator _cryptoGen = RandomNumberGenerator.Create();
+        private readonly Random _random = new Random();
+        
+        public ApiKeyGenerator(FeedRepoDatabase db)
+        {
+            _db = db;
+        }
+
+        public string GenerateNew()
+        {
+            var data = new byte[32];
+            _cryptoGen.GetBytes(data);
+            var b64 = Convert.ToBase64String(data);
+            var idSb = new StringBuilder(b64, 46);
+            idSb.Replace('+', (char)_random.Next(97, 122));
+            idSb.Replace('=', (char)_random.Next(97, 122));
+            idSb.Replace('/', (char)_random.Next(97, 122));
+            var id = idSb.ToString();
+            return _db.Feeds.FindOne(id) == null 
+                ? id 
+                : GenerateNew();
+        }
+    }
+}
--- TOIFeedRepo/FeedRepo.cs
+++ TOIFeedRepo/FeedRepo.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using FormValidator;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 using RedHttpServerCore;
 using TOIClasses;
 using TOIFeedRepo.Database;
@@ -18,14 +19,17 @@ namespace TOIFeedRepo
         {
             _server = new RedHttpServer(port, "./public");
 
-            _server.Get("hello", async (req, res) =>
-            {
-                await res.SendString("Hellow from the feed repo!");
-            });
-
-            var db = FeedRepoDatabaseFactory.Build(development ? DatabaseFactory.DatabaseType.InMemory : DatabaseFactory.DatabaseType.MongoDB);
+            var db = FeedRepoDatabaseFactory.Build(development 
+                ? DatabaseFactory.DatabaseType.InMemory 
+                : DatabaseFactory.DatabaseType.MongoDB);
 
             var fMan = new FeedServerManager(db);
+            var auth = new Authenticator(db);
+            
+            
+            
+            
+            
             _server.Get("/feeds", async (req, res) =>
             {
                 var feeds = await fMan.AllActiveFeeds();
@@ -65,11 +69,107 @@ namespace TOIFeedRepo
                     await res.SendString(createFeedResult.Message, status: StatusCodes.Status400BadRequest);
                 }
             });
+            
+            _server.Get("/feed/status", async (req, res) =>
+            {
+                if (!req.Queries.TryGetValue("apikey", out var apiKey))
+                {
+                    await res.SendString("Invalid request (missing API key)", status: 401);
+                    return;
+                }
+                var server = auth.GetFeedServer(apiKey);
+                
+                if (server != null)
+                    await res.SendJson(server);
+                else
+                    await res.SendString("Invalid request (invalid API key)", status: 404);
+
+            });
+            _server.Post("/feed/update", async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                var createFeedResult = await fMan.UpdateFeed(form);
+                if (server != null)
+                    await res.SendJson(server);
+                else
+                    await res.SendString("Invalid request (invalid API key)", status: 404);
+
+            });
+            _server.Post("/feed/create", async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                var createFeedResult = await fMan.RegisterFeed(form);
+
+                if (createFeedResult.Result != null)
+                {
+                    await res.SendJson(createFeedResult.Result);
+                }
+                else
+                {
+                    await res.SendString(createFeedResult.Message, status: StatusCodes.Status400BadRequest);
+                }
+            });
+            
+            _server.Post("/requestapikey", async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                var ok = auth.RequestApiKey(form);
+
+            });
         }
 
         public void Start()
         {
             _server.Start();
         }
+    }
+
+    class Authenticator
+    {
+        private ApiKeyGenerator _keygen;
+        private FormValidator.FormValidator _requestFormValidator;
+        private FeedRepoDatabase _db;
+
+        public Authenticator(FeedRepoDatabase db)
+        {
+            _db = db;
+            _keygen = new ApiKeyGenerator(db);
+            _requestFormValidator = FormValidatorBuilder.New()
+                .RequiresString("email")
+                .RequiresString("type")
+                .RequiresString("name")
+                .RequiresString("street")
+                .RequiresString("zip", 4)
+                .RequiresString("city")
+                .RequiresString("country")
+                .Build();
+
+        }
+
+        public bool RequestApiKey(IFormCollection form)
+        {
+            if (!_requestFormValidator.Validate(form))
+                return false;
+            var customer = new Customer();
+            
+        }
+
+
+        public Feed GetFeedServer(StringValues apiKey)
+        {
+            return _db.Feeds.FindOne(apiKey);
+            
+        }
+    }
+
+    class Customer
+    {
+        public string Email { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 }
